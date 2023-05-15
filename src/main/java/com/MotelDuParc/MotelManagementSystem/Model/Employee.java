7d6e17181788b7d0340a10b716ea435f4a305478
leng0013 <xu.leng@outlook.com>
2023-05-15 15:21:16 -0400

    FINISH MODEL OF Employee.java, Guest.java, Room.java

--- src/main/java/com/MotelDuParc/MotelManagementSystem/Model/Employee.java
+++ src/main/java/com/MotelDuParc/MotelManagementSystem/Model/Employee.java
@@ -1,11 +1,89 @@
 package com.MotelDuParc.MotelManagementSystem.Model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
 public class Employee implements Serializable {
+
+    public Employee() {
+
+    }
+
+    public Employee(long id, String firstName, String lastName, String phoneNumber, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private long id;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false)
     private String phoneNumber;
+    @Column(nullable = false)
     private String address;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
